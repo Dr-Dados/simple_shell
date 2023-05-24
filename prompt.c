83c81ebaf4c4bad516c6403bfb944ec9a7bfb8b0
Dr-Dados <adnane.bachchar@gmail.com>
2023-05-24 00:35:52 -0700

    prompt.c updated

--- prompt.c
+++ prompt.c
@@ -1,5 +1,6 @@
 #include "shell.h"
 #include <sys/wait.h>
+
 /**
  * prompt - Display a prompt and execute commands entered by the user.
  * @av: Array of strings representing command-line arguments.
@@ -7,23 +8,18 @@
  */
 void prompt(char **av, char **env)
 {
-char *str = NULL;
-int i, status;
+char *str = NULL, *argv[MAX_COMMAND];
+int i, status, j;
 size_t n = 0;
 ssize_t sz;
-char *argv[] = {NULL, NULL};
 pid_t ch_pid;
-
 while (1)
 {
-  /**if (isatty(STDIN_FILENO))*/
+if (isatty(STDIN_FILENO))
 printf("cisfun$ ");
 sz = getline(&str, &n, stdin);
 if (sz == -1)
-{
-free(str);
-exit(EXIT_FAILURE);
-}
+break;
 i = 0;
 while (str[i])
 {
@@ -31,13 +27,13 @@ if (str[i] == '\n')
 str[i] = 0;
 i++;
 }
-argv[0] = str;
+j = 0;
+argv[j] = strtok(str, " ");
+while (argv[j])
+argv[++j] = strtok(NULL, " ");
 ch_pid = fork();
 if (ch_pid == -1)
-{
-free(str);
-exit(EXIT_FAILURE);
-}
+break;
 if (ch_pid == 0)
 {
 if (execve(argv[0], argv, env) == -1)
@@ -46,4 +42,6 @@ printf("%s: No such file or directory\n", av[0]);
 else
 wait(&status);
 }
+free(str);
+exit(EXIT_FAILURE);
 }

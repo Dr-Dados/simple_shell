a437a758f7d16b7d26f79a20557cf7c93ab3cf99
Dr-Dados <adnane.bachchar@gmail.com>
2023-05-24 00:48:27 -0700

    prompt updated

--- prompt.c
+++ prompt.c
@@ -8,11 +8,12 @@
  */
 void prompt(char **av, char **env)
 {
-char *str = NULL, *argv[MAX_COMMAND];
-int i, status, j;
+char *str = NULL, *argv[MAX_COMMAND] ,*token;
+int status,j;
 size_t n = 0;
 ssize_t sz;
 pid_t ch_pid;
+
 while (1)
 {
 if (isatty(STDIN_FILENO))
@@ -20,20 +21,22 @@ printf("cisfun$ ");
 sz = getline(&str, &n, stdin);
 if (sz == -1)
 break;
-i = 0;
-while (str[i])
-{
-if (str[i] == '\n')
-str[i] = 0;
-i++;
-}
+str[sz - 1] = '\0';
 j = 0;
-argv[j] = strtok(str, " ");
-while (argv[j])
-argv[++j] = strtok(NULL, " ");
+token = strtok(str, " ");
+while (token != NULL && j < MAX_COMMAND - 1)
+{
+argv[j] = token;
+token = strtok(NULL, " ");
+j++;
+}
+argv[j] = NULL;
 ch_pid = fork();
 if (ch_pid == -1)
+{
+perror("fork");
 break;
+}
 if (ch_pid == 0)
 {
 if (execve(argv[0], argv, env) == -1)

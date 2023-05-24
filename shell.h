2dc256e6ab98f12d789649bf4adbd469b35c0b21
Dr-Dados <adnane.bachchar@gmail.com>
2023-05-24 00:38:19 -0700

    prompt updated

--- shell.h
+++ shell.h
@@ -1,10 +1,13 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
+#define MAX_COMMAND 10
 void prompt(char **av, char **env);
 
 #endif
